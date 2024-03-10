fd5af9f16cf86281ac1113a60d8ffa7c0f6712cc
JingyangQiao <159891886+JingyangQiao@users.noreply.github.com>
2024-03-10 21:25:58 +0800

    Update models.py

--- l2p-pgp/models.py
+++ l2p-pgp/models.py
@@ -38,5 +38,5 @@ def vit_base_patch16_224(pretrained=False, **kwargs):
     ImageNet-1k weights fine-tuned from in21k @ 224x224, source https://github.com/google-research/vision_transformer.
     """
     model_kwargs = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, **kwargs)
-    model = _create_vision_transformer('vit_base_patch16_224_sam', pretrained=pretrained, **model_kwargs)
+    model = _create_vision_transformer('vit_base_patch16_224', pretrained=pretrained, **model_kwargs)
     return model
